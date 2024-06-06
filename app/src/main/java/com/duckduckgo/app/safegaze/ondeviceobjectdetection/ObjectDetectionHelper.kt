4078a99e59874e2a6aeb43b9ee2812a9d177f618
Tawfiqur <xiifwat@gmail.com>
2024-06-06 15:09:43 +0600

    Instead of loading the person detection model from asset, use it with model binding

--- app/src/main/java/com/duckduckgo/app/safegaze/ondeviceobjectdetection/ObjectDetectionHelper.kt
+++ app/src/main/java/com/duckduckgo/app/safegaze/ondeviceobjectdetection/ObjectDetectionHelper.kt
@@ -2,9 +2,8 @@ package com.duckduckgo.app.safegaze.ondeviceobjectdetection
 
 import android.content.Context
 import android.graphics.Bitmap
+import com.duckduckgo.app.browser.ml.DetectPerson
 import org.tensorflow.lite.support.image.TensorImage
-import org.tensorflow.lite.task.vision.detector.Detection
-import org.tensorflow.lite.task.vision.detector.ObjectDetector
 
 /**
  * Created by Asif Ahmed on 2/1/24.
@@ -12,36 +11,19 @@ import org.tensorflow.lite.task.vision.detector.ObjectDetector
 
 class ObjectDetectionHelper(val context: Context) {
 
-    /**
-     * ML Kit Object Detection function.
-     */
-    private fun runObjectDetectionWithCustomModel(bitmap: Bitmap): MutableList<Detection> {
-
-        // Step 1: create TFLite's TensorImage object
-        val image = TensorImage.fromBitmap(bitmap)
-
-        // Step 2: Initialize the detector object
-        val options = ObjectDetector.ObjectDetectorOptions.builder()
-            .setMaxResults(5)
-            .setScoreThreshold(0.5f)
-            .build()
-        val detector = ObjectDetector.createFromFileAndOptions(
-            context, // the application context
-            "detect_person.tflite", // must be same as the filename in assets folder
-            options,
-        )
-
-        // Step 3: feed given image to the model and print the detection result
-        val results = detector.detect(image)
-
-        return results
-    }
+    var model: DetectPerson = DetectPerson.newInstance(context)
 
     fun isImageContainsHuman(bitmap: Bitmap): Boolean {
-        return runObjectDetectionWithCustomModel(bitmap).any { detection ->
-            detection.categories.any { category ->
-                category.label.equals("person", ignoreCase = true)
-            }
+        val image = TensorImage.fromBitmap(bitmap)
+        val outputs = model.process(image)
+        val detectionsList = outputs.detectionResultList
+
+        return detectionsList.any { detection ->
+            detection.categoryAsString.equals("person", ignoreCase = true)
         }
+    }
+
+    fun dispose() {
+        model.close()
     }
 }
